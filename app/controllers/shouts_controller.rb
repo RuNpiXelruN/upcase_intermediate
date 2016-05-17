783297cc2307389d450a17999610aebeca47101a
Justin Davidson <jutstindavidson23@gmail.com>
2016-05-17 16:10:27 +1000

    Mid-video2, added polymorphic for text_shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -3,20 +3,4 @@ class ShoutsController < ApplicationController
   def show
     @shout = Shout.find(params[:id])
   end
-
-  def create
-    @shout = current_user.shouts.build(shout_params)
-    if @shout.save
-      flash[:notice] = "Shout successfully posted!"
-      redirect_to dashboard_path
-    else
-      flash[:notice] = "Something went wrong :("
-      redirect_to :back
-    end
-  end
-
-  private
-  def shout_params
-    params.require(:shout).permit(:body)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   root 'homes#show', via: :get
-  resource :dashboard, only: :show
+  resource :dashboard, only: [:show]
   devise_for :users
-  resources :shouts, only: [:create, :show]
+  resources :shouts, only: [:show]
+  resources :text_shouts, only: :create
   match 'users/show', to: 'users#show', via: 'get'
 
 
